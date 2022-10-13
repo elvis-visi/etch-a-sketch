0417816dfe2ce57467f6b17b80984f8c2f82fd2a
seviLT <98219835+seviLT@users.noreply.github.com>
2022-10-13 12:35:24 +0100

    4.initialize 16x16 grid, prompt user to change grid size

--- app.js
+++ app.js
@@ -3,13 +3,8 @@
 //place the grid squares inside container div from index.html
 
 const container = document.querySelector('#container');
-
-
-//hover effect that grid divs change color 
-
-//event lsitener for when you enter and leave a div?
-
-//changing the div’s background color using JavaScript.
+const divMain =  document.createElement('div')
+divMain.classList.add('divMain')
 
 function mouseEvent(event) {
 
@@ -23,18 +18,77 @@ function mouseEvent(event) {
 
 }
 
+container.appendChild(divMain);
 
-for(let i =  1; i < 257; i++)
+const button = document.querySelector('#btn');
+button.classList.add('btn');
+
+//initial grid 16x16
+generateGrid(16)
+
+
+//prompt for square numbers
+
+//if > 100, prompt again 0-100 choice
+
+//save prompt return to a variable.
+
+//remove existing grid, new grid generated
+
+function gridNumber() {
+
+    let gridNumber = -1;  
+
+    while(gridNumber < 1 || gridNumber > 100 )
+    {
+       gridNumber =  prompt('Number of squares per side');
+    }
+
+    //remove current grid, remove all children of divMain
+    let parent = document.querySelector('.divMain');
+    console.log("parent", parent)
+    while(parent.firstChild){
+        parent.removeChild(parent.firstChild)
+    }
+
+    //add new grid
+    generateGrid(gridNumber)
+}
+
+button.addEventListener('click',gridNumber)
+
+function generateGrid(gridNumber)
+{
+
+    //grid-template-columns: repeat(  16, 1fr);
+
+    divMain.style.cssText = `grid-template-columns: repeat(  ${gridNumber}, 1fr);`
+    for(let i =  1; i < gridNumber * gridNumber + 1; i++)
 {
     let div = document.createElement('div');  
     div.textContent= `div ${i}`  
 
     div.addEventListener('mouseover' , mouseEvent)
     
-
     //add a class for default/mouseout 
 
     div.addEventListener('mouseout' , mouseEvent)
 
-    container.appendChild(div);  
+    divMain.appendChild(div);  
 }
+}
+
+/*for(let i =  1; i < 257; i++)
+{
+    let div = document.createElement('div');  
+    div.textContent= `div ${i}`  
+
+    div.addEventListener('mouseover' , mouseEvent)
+    
+    //add a class for default/mouseout 
+
+    div.addEventListener('mouseout' , mouseEvent)
+
+    divMain.appendChild(div);  
+}
+*/
