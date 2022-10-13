f757d59cf54cf74f78147e169344cde0f74b1b0c
seviLT <98219835+seviLT@users.noreply.github.com>
2022-10-13 09:53:11 +0100

    2. mouseOver/mouseOut event Listeners

--- app.js
+++ app.js
@@ -5,13 +5,27 @@
 const container = document.querySelector('#container');
 
 
+//hover effect that grid divs change color 
+
+//event lsitener for when you enter and leave a div?
+
+//changing the div’s background color using JavaScript.
+
 
 for(let i =  1; i < 257; i++)
 {
     let div = document.createElement('div');  
     div.textContent= `div ${i}`  
 
-   
+    div.addEventListener('mouseover' , () => {
+        div.style.backgroundColor = 'blue';
+    })
+
+    //add a class for default/mouseout 
+
+    div.addEventListener('mouseout' , () => {
+        div.style.backgroundColor = 'white';
+    })
 
     container.appendChild(div);  
 }
