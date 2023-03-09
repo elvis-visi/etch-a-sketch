cbbeae3311c5dd05496105635f42dc3e2724ee81
elvis-visi <sivisevi1@gmail.com>
2023-03-09 17:25:39 +0000

    3. change color when your mouse passes over a cell.

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ grid.classList.add('gridContainer')
 for(let i = 0; i< 256; i++)
 {
     const cell = document.createElement('div');
+    cell.classList.add('cell');
     grid.appendChild(cell);
 }
 
