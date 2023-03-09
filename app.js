7812087d16e0ebfefb994d7ec0883136e8373278
elvis-visi <sivisevi1@gmail.com>
2023-03-09 17:33:09 +0000

    4. hover effect using event listeners mouseon and mouseout.

--- app.js
+++ app.js
@@ -10,6 +10,16 @@ for(let i = 0; i< 256; i++)
     const cell = document.createElement('div');
     cell.classList.add('cell');
     grid.appendChild(cell);
+
+
+    //add mouseover event listener to change cell color
+    cell.addEventListener('mouseover', () => {
+        cell.style.backgroundColor = 'black';
+    })
+
+    cell.addEventListener('mouseout', () => {
+        cell.style.backgroundColor = 'white';
+    });
 }
 
 container.appendChild(grid);
