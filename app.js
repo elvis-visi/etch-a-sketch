9f2134514770365b511ab31efe9d2669e7cdbca4
elvis-visi <sivisevi1@gmail.com>
2023-03-09 17:58:17 +0000

    5. Function to reset and generate new grid, default size 16x16.

--- app.js
+++ app.js
@@ -4,22 +4,43 @@ const container = document.querySelector('#mainContainer')
 const grid = document.createElement('div');
 grid.classList.add('gridContainer')
 
-//16x16 grid 
-for(let i = 0; i< 256; i++)
-{
-    const cell = document.createElement('div');
-    cell.classList.add('cell');
-    grid.appendChild(cell);
+
+//default grid size 16
+let gridSize = 16;
+
+//generate new grid. 
+function generateGrid() {
+
+    //check if the grid element has any child elements, remove them if it does
+    while(grid.firstChild) {
+        grid.removeChild(grid.firstChild);
+    }
+
+    //we empty the grid above, resetting it.
+
+     // Generate new grid
+        for(let i = 0; i< gridSize * gridSize; i++)
+    {
+        const cell = document.createElement('div');
+        cell.classList.add('cell');
+        grid.appendChild(cell);
 
 
-    //add mouseover event listener to change cell color
-    cell.addEventListener('mouseover', () => {
-        cell.style.backgroundColor = 'black';
-    })
+        //add mouseover event listener to change cell color
+        cell.addEventListener('mouseover', () => {
+            cell.style.backgroundColor = 'black';
+        })
 
-    cell.addEventListener('mouseout', () => {
-        cell.style.backgroundColor = 'white';
-    });
+        cell.addEventListener('mouseout', () => {
+            cell.style.backgroundColor = 'white';
+        });
+    }
+
+    container.appendChild(grid);
+
 }
 
-container.appendChild(grid);
+generateGrid()
+
+
+
